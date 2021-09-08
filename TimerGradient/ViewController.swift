d95e2a180570770d6a026c98bc32c7f9c2a2a625
HiroUrata <1712026@i.softbank.jp>
2021-09-08 17:32:11 +0900

    Timer( )でCAGradientLayer( )のcolorをランダムで決定させる

--- TimerGradient/ViewController.swift
+++ TimerGradient/ViewController.swift
@@ -9,23 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var gradient = CAGradientLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
-        Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector(changeGradient), userInfo: nil, repeats: true)
+        
+        gradient.frame = CGRect(x: self.view.frame.minX, y: self.view.frame.minY, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        
+        Timer.scheduledTimer(timeInterval: 4, target: self, selector: #selector(changeGradient), userInfo: nil, repeats: true)
         
     }
 
     @objc func changeGradient(){
         
-        UIView.animate(withDuration: 2.0, animations: {
+        UIView.animate(withDuration: 2,delay: 0,options: .allowUserInteraction ,animations: {[self] in
             
-            self.view.backgroundColor = UIColor(red: CGFloat.random(in: 0...255) / 255,
-                                                                        green: CGFloat.random(in: 0...255) / 255,
-                                                                        blue: CGFloat.random(in: 0...255) / 255,
-                                                                        alpha: CGFloat.random(in: 0.0...1.0))
+            gradient.colors = [UIColor(red: CGFloat.random(in: 0...255) / 255,green:CGFloat.random(in: 0...255) / 255,blue: CGFloat.random(in: 0...255) / 255,alpha: CGFloat.random(in: 0.0...1.0)).cgColor,UIColor(red: CGFloat.random(in: 0...255) / 255,green:CGFloat.random(in: 0...255) / 255,blue: CGFloat.random(in: 0...255) / 255,alpha: CGFloat.random(in: 0.0...1.0)).cgColor]
             
-        })
+            gradient.startPoint = CGPoint(x: 0.5, y: 0.5)
+            gradient.endPoint = CGPoint(x: 0.5, y: 0)
+            self.view.layer.insertSublayer(gradient, at: 0)
+                
+        },completion: nil)
     }
 
 }
