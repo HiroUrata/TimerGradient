1e7a95a859a9709aa1ad858a8af145ea27f8e136
HiroUrata <1712026@i.softbank.jp>
2021-09-08 16:50:22 +0900

    ランダムで背景色を変わるようにする

--- TimerGradient/ViewController.swift
+++ TimerGradient/ViewController.swift
@@ -11,9 +11,22 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector(changeGradient), userInfo: nil, repeats: true)
+        
     }
 
+    @objc func changeGradient(){
+        
+        UIView.animate(withDuration: 2.0, animations: {
+            
+            self.view.backgroundColor = UIColor(red: CGFloat.random(in: 0...255) / 255,
+                                                                        green: CGFloat.random(in: 0...255) / 255,
+                                                                        blue: CGFloat.random(in: 0...255) / 255,
+                                                                        alpha: CGFloat.random(in: 0.0...1.0))
+            
+        })
+    }
 
 }
 
