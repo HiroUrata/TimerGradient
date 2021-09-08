1147c6a3b62acd218ec0d91df32c1f86e2b7da66
HiroUrata <1712026@i.softbank.jp>
2021-09-08 16:45:17 +0900

    Initial Commit

---
+++ TimerGradient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TimerGradient
+//
+//  Created by UrataHiroki on 2021/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
